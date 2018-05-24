75b4e0b49afbfaaf48619860bbb82df1301c020b
wenbchen <wenbchen@microstrategy.com>
2018-05-24 17:40:10 -0400

    add javadoc for android library module project

--- src/main/groovy/nebula/plugin/javadoc/NebulaAggregateJavadocPlugin.groovy
+++ src/main/groovy/nebula/plugin/javadoc/NebulaAggregateJavadocPlugin.groovy
@@ -14,22 +14,31 @@ class NebulaAggregateJavadocPlugin implements Plugin<Project> {
         Project rootProject = project.rootProject
         rootProject.gradle.projectsEvaluated {
             Set<Project> javaSubprojects = getJavaSubprojects(rootProject)
-            if (!javaSubprojects.isEmpty()) {
+            Set<Project> librarySubprojects = getLibrarySubprojects(rootProject)
+            if (!javaSubprojects.isEmpty() || !librarySubprojects.isEmpty()) {
                 rootProject.task(AGGREGATE_JAVADOCS_TASK_NAME, type: Javadoc) {
                     description = 'Aggregates Javadoc API documentation of all subprojects.'
                     group = JavaBasePlugin.DOCUMENTATION_GROUP
                     dependsOn javaSubprojects.javadoc
+                    dependsOn librarySubprojects.generateReleaseJavadoc
 
                     source javaSubprojects.javadoc.source
+                    source librarySubprojects.generateReleaseJavadoc.source
                     destinationDir rootProject.file("$rootProject.buildDir/docs/javadoc")
                     classpath = rootProject.files(javaSubprojects.javadoc.classpath)
+                    classpath += rootProject.files(librarySubprojects.generateReleaseJavadoc.classpath)
                 }
             }
         }
     }
 
-    private Set<Project> getJavaSubprojects(Project rootProject) {
-        rootProject.subprojects.findAll { subproject ->
-            subproject.plugins.hasPlugin(JavaPlugin) || subproject.plugins.findPlugin("com.android.library")}
+    private Set<Project> getLibrarySubprojects(Project rootProject) {
+        rootProject.subprojects.findAll {subproject -> subproject.plugins.findPlugin("com.android.library")}
     }
+
+    private Set<Project> getJavaSubprojects(Project rootProject) {
+        rootProject.subprojects.findAll { subproject -> subproject.plugins.hasPlugin(JavaPlugin)}
+    }
+
+
 }
